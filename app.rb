d15e7c4ac19adacb8bf21814f69e56f8265f04b3
Ashwin Kannan <classifieddatabank@Ashwins-MacBook-Pro.local>
2019-09-17 16:32:01 -0500

    lab complete

--- app.rb
+++ app.rb
@@ -1,9 +1,24 @@
 require './environment'
+require 'pry'
 
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :index
+    end
 
+    get '/new' do
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do
+      @p_name = params[:pirate][:name]
+      @p_height = params[:pirate][:height]
+      @p_weight = params[:pirate][:weight]
+      @s1 = params[:pirate][:ships][0]
+      @s2 = params[:pirate][:ships][1]
+      erb :"pirates/show"
+    end
   end
 end
