e88235d360cc43a5c49fb65471336968c29d5106
zSteinO34 <zstein1234@gmail.com>
2019-09-17 17:16:08 -0500

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,21 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do 
+      erb :root
+    end
+
+    get '/new' do
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do 
+      @pirate = params[:pirate]
+      p @pirate
+      @ship1 = @pirate[:ships][0]
+      @ship2 = @pirate[:ships][1]
+      erb :"pirates/show"
+    end
 
   end
 end
